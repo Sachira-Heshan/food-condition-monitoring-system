f7d956ad9c6268b38573eb439b139bb9019e72e0
Sachira-Heshan <sachiraheshan99@gmail.com>
2024-08-05 02:30:50 +0530

    Sensor reading stored in db endpoints working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,14 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={" bg-neutral-900 h gap-2 flex p-2  "}>
-            <div className={"h-full w-1/4 min-w-[304px]"}>
-               <SideBar />
-            </div>
-            <div className={"h-full w-full min-w-[304px]"}>
-               <div className={"w-full py-2"}>
-                  <Dashboard />
+            <div className={inter.className}>
+               <div className={"h-full w-1/4 min-w-[304px]"}>
+                  <SideBar />
+               </div>
+               <div className={"h-full w-full min-w-[304px]"}>
+                  <div className={"w-full py-2"}>
+                     <Dashboard />
+                  </div>
                </div>
             </div>
          </body>
